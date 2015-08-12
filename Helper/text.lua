eeac0ace73bc53a03a7f3bdef92df0bef795271b
Kryssi79 <kryssilisty@freenet.de>
2015-08-12 13:41:50 +0200

    Text Helper functionen

---
+++ Helper/text.lua
@@ -0,0 +1,26 @@
+
+
+function centerText(m, text, dy)
+  local x,y=m.getSize()
+  center = math.ceil((x/2) - (text:len() / 2))
+  if dy == nil then
+    m.setCursorPos(center, math.ceil(y/2))
+  elseif dy > 0 then
+    m.setCursorPos(center, dy)
+  end
+  return center
+end
+
+
+function rightJustify(m, text, dy)
+  local x,y=m.getSize()
+  right = x-text:len()
+  if dy == nil then
+    m.setCursorPos(right, math.ceil(y/2))
+  elseif dy > 0 then
+    m.setCursorPos(right, dy)
+  end
+  return center
+end
+
+

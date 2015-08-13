5b0a5e1ce4e12aabb29d1008a37df86ed39de9d6
Kryssi79 <kryssilisty@freenet.de>
2015-08-13 09:13:51 +0200

    more colors

--- BCtest/showAsBool.lua
+++ BCtest/showAsBool.lua
@@ -4,8 +4,8 @@ print(" CTRL + T  Terminate script")
 mon = peripheral.wrap("top")
 mon.clear()
 mon.setCursorPos(1,1)
-mon.write("bundled cable AsBool  Ver 0.4")
-startYPos = 2
+mon.write("bundled cable AsBool  Ver 0.5")
+startYPos = 3
 
 
 function ShowCableAsBool(sText, cFarbe, yPos)
@@ -19,7 +19,12 @@ end
 
 while true do
   bndCable = rs.getBundledInput("bottom")
-  ShowCableAsBool("gelb: ", colors.yellow, 3)
+  ShowCableAsBool("    white: ", colors.white, startYPos+0)
+  ShowCableAsBool("   orange: ", colors.orange, startYPos+1)
+  ShowCableAsBool("  magenta: ", colors.magenta, startYPos+2)
+  ShowCableAsBool("lightBlue: ", colors.lightBlue, startYPos+3)
+  ShowCableAsBool("   yellow: ", colors.yellow, startYPos+4)
+  ShowCableAsBool("     lime: ", colors.lime, startYPos+5)
   os.sleep(1)
 end
 
